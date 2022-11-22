1a2b5eeee1929c9f31077e4ee2b0ef715420b2f3
angelsornichero <angelsornicheropacheco2008@gmail.com>
2022-11-22 15:56:50 +0100

    Functions of database

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE FilesApp;
+
+USE FilesApp;
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+"""USERS FOR TESTING"""
+INSERT INTO users(username, name, password, email) VALUES ('user_test', 'test', 'test', 'test@test.com');
